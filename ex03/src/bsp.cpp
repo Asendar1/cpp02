881537f8fc07f5b2ccfa0dbb776cd2c6ea0aa051
Asendar1 <hamzahtaimour@gmail.com>
2025-09-28 19:52:58 +0300

    better readbility

--- ex03/src/bsp.cpp
+++ ex03/src/bsp.cpp
@@ -9,8 +9,7 @@ bool	bsp(Point const a, Point const b, Point const c, Point const point)
 	float d2 = ((point.getX().toFloat() - c.getX().toFloat()) * (b.getY().toFloat() - c.getY().toFloat()) - (b.getX().toFloat() - c.getX().toFloat()) * (point.getY().toFloat() - c.getY().toFloat()));
 	float d3 = ((point.getX().toFloat() - a.getX().toFloat()) * (c.getY().toFloat() - a.getY().toFloat()) - (c.getX().toFloat() - a.getX().toFloat()) * (point.getY().toFloat() - a.getY().toFloat()));
 
-	bool has_neg = (d1 < 0) || (d2 < 0) || (d3 < 0);
-	bool has_pos = (d1 > 0) || (d2 > 0) || (d3 > 0);
-
-	return (!(has_neg && has_pos));
+	if ((d1 > 0 && d2 > 0 && d3 > 0) || (d1 < 0 && d2 < 0 && d3 < 0))
+		return true;
+	return false;
 }
